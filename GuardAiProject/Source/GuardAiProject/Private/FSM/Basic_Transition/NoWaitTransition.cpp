5a2ded68c4b6446cec1a1a6677047bb30e07d173
Domergue31 <114091389+Domergue31@users.noreply.github.com>
2024-05-02 09:46:58 +0200

    trgrgf

--- GuardAiProject/Source/GuardAiProject/Private/FSM/Basic_Transition/NoWaitTransition.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "FSM/Basic_Transition/NoWaitTransition.h"
-
-bool UNoWaitTransition::IsValid()
-{
-	return true;
-}
